ea8fad1bedd04167e533f992aba45dbbdeb4ffff
mostovarpour <matt.ostovarpour@gmail.com>
2015-12-01 13:21:52 -0700

    Put in the bare basics.

---
+++ Client.java
@@ -0,0 +1,13 @@
+/*
+ * Matthew Ostovarpour
+ * Daniel Durazo
+ * 12/10/15
+ * File Server Client in Java
+ */
+
+import java.net.*;
+import java.io.*;
+
+public class Client{
+
+}
---
+++ Server.java
@@ -0,0 +1,13 @@
+/*
+ * Matthew Ostovarpour
+ * Daniel Durazo
+ * 12/10/15
+ * File Server Server in Java
+ */
+
+import java.net.*;
+import java.io.*;
+
+public class Server{
+
+}

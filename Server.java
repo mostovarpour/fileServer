903b707936045cc75e5126bbda2d5a43b145af10
mostovarpour <matt.ostovarpour@gmail.com>
2015-12-08 13:24:42 -0700

    Added in core functionality including the menu and allowing the client and server to communicate.

--- Server.java
+++ Server.java
@@ -9,5 +9,26 @@ import java.net.*;
 import java.io.*;
 
 public class Server{
+    private static ServerSocket serverSocket;
+    private static Socket clientSocket = null;
 
+    public static main void(String[] args) throws IOException{
+        try{
+            serverSocket = new ServerSocket(4444);
+            System.out.println("Server Started.");
+        } catch (Exception e){
+            System.err.println("Port already in use.");
+            System.exit(1);
+        }
+        while (true){
+            try{
+                clientSocket = serverSocket.accept();
+                System.out.println("Accepted connection: " + clientSocket);
+                Thread t = new Thread(new CLIENTConnection(clientSocket));
+                t.start();
+            } catch (Exception e){
+                System.err.println("Error in connection attempt.");
+            }
+        }
+    }
 }
